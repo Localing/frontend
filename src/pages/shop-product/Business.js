f6a639ba9bdd1e60dd169b6f31f87dc093577592
Terrell Ibanez <9453223+ibanezt@users.noreply.github.com>
2020-05-03 14:08:02 -0400

    Add business page

---
+++ src/pages/shop-product/Business.js
@@ -0,0 +1,61 @@
+import PropTypes from "prop-types";
+import React, { Fragment } from "react";
+import MetaTags from "react-meta-tags";
+import { BreadcrumbsItem } from "react-breadcrumbs-dynamic";
+import { connect } from "react-redux";
+import LayoutOne from "../../layouts/LayoutOne";
+import Breadcrumb from "../../wrappers/breadcrumb/Breadcrumb";
+import RelatedProductSlider from "../../wrappers/product/RelatedProductSlider";
+import ProductDescriptionTab from "../../wrappers/product/ProductDescriptionTab";
+import BusinessDescription from "../../wrappers/product/BusinessDescription";
+
+const Business
+  = ({ location, product }) => {
+    const { pathname } = location;
+
+    return (
+      <Fragment>
+        <MetaTags>
+          <title>Flone | Business Page</title>
+          <meta
+            name="description"
+            content="Business page"
+          />
+        </MetaTags>
+
+        <BreadcrumbsItem to={process.env.PUBLIC_URL + "/"}>Home</BreadcrumbsItem>
+        <BreadcrumbsItem to={process.env.PUBLIC_URL + pathname}>
+          Business
+      </BreadcrumbsItem>
+
+        <LayoutOne headerTop="visible">
+          {/* breadcrumb */}
+          <Breadcrumb />
+
+          {/* product description with image */}
+          <BusinessDescription
+            spaceTopClass="pt-100"
+            spaceBottomClass="pb-100"
+            product={product}
+            galleryType="fixedImage"
+          />
+        </LayoutOne>
+      </Fragment>
+    );
+  };
+
+Business.propTypes = {
+  location: PropTypes.object,
+  product: PropTypes.object
+};
+
+const mapStateToProps = (state, ownProps) => {
+  const itemId = ownProps.match.params.id;
+  return {
+    product: state.productData.products.filter(
+      single => single.id === itemId
+    )[0]
+  };
+};
+
+export default connect(mapStateToProps)(Business);
