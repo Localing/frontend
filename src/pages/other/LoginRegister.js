87804ca9cf61597cfadd97a2794ba6b2fc3388db
Vishnu Ravi <vishnu.ravi@gmail.com>
2020-05-26 17:01:14 -0400

    Revert to prior login form, change auth domain

--- src/pages/other/LoginRegister.js
+++ src/pages/other/LoginRegister.js
@@ -11,16 +11,15 @@ import Breadcrumb from "../../wrappers/breadcrumb/Breadcrumb";
 import { Auth } from 'aws-amplify';
 import { connect } from 'react-redux';
 import { loginUser } from '../../redux/actions/authActions';
-import { AmplifyAuthenticator, AmplifySignUp } from '@aws-amplify/ui-react';
 
-const LoginRegister = ({
-  location,
+const LoginRegister = ({ 
+  location, 
   loginUser,
   loginError,
   isAuthenticated }) => {
   const { pathname } = location;
 
-  let { from } = location.state || { from: { pathname: "/" } };
+  let { from } = location.state || { from: { pathname: "/" }};
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -44,47 +43,126 @@ const LoginRegister = ({
   return (
     isAuthenticated ?
       <Redirect to={from} />
-      :
-      <Fragment>
-        <MetaTags>
-          <title>Localing | Login</title>
-          <meta
-            name="description"
-            content="Localing Login"
-          />
-        </MetaTags>
-        <BreadcrumbsItem to={process.env.PUBLIC_URL + "/"}>Home</BreadcrumbsItem>
-        <BreadcrumbsItem to={process.env.PUBLIC_URL + pathname}>
-          Login &amp; Register
+    :
+    <Fragment>
+      <MetaTags>
+        <title>Localing | Login</title>
+        <meta
+          name="description"
+          content="Localing Login"
+        />
+      </MetaTags>
+      <BreadcrumbsItem to={process.env.PUBLIC_URL + "/"}>Home</BreadcrumbsItem>
+      <BreadcrumbsItem to={process.env.PUBLIC_URL + pathname}>
+        Login &amp; Register
       </BreadcrumbsItem>
-        <LayoutOne headerTop="visible">
-          {/* breadcrumb */}
-          <div className="login-register-area pt-100 pb-100">
-            <div className="container">
-              <div className="row">
-                <div className="col-lg-7 col-md-12 ml-auto mr-auto">
-                  <div className="login-register-wrapper">
-                    <button onClick={() => Auth.federatedSignIn({ provider: 'Facebook' })}>Open Facebook</button>
-                    <button onClick={() => Auth.federatedSignIn({ provider: 'Google' })}>Open Google</button>
-                    <AmplifyAuthenticator usernameAlias="email">
-                    </AmplifyAuthenticator>
-                  </div>
+      <LayoutOne headerTop="visible">
+        {/* breadcrumb */}
+        <Breadcrumb />
+        <div className="login-register-area pt-100 pb-100">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-7 col-md-12 ml-auto mr-auto">
+                <div className="login-register-wrapper">
+                  <Tab.Container defaultActiveKey="login">
+                    <Nav variant="pills" className="login-register-tab-list">
+                      <Nav.Item>
+                        <Nav.Link eventKey="login">
+                          <h4>Login</h4>
+                        </Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="register">
+                          <h4>Register</h4>
+                        </Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content>
+                      <Tab.Pane eventKey="login">
+                        <div className="login-form-container">
+                          <div className="login-register-form">
+                          <Button onClick={() => Auth.federatedSignIn({ provider: 'Facebook' })} block>Login with Facebook</Button>
+                          <Button onClick={() => Auth.federatedSignIn({ provider: 'Google' })} block>Login with Google</Button>
+                          <br />
+                            <form onSubmit={handleSubmit}>
+                              <input
+                                type="text"
+                                name="email"
+                                placeholder="Email"
+                                value={email}
+                                onChange={handleChange}
+                              />
+                              <input
+                                type="password"
+                                name="password"
+                                placeholder="Password"
+                                value={password}
+                                onChange={handleChange}
+                              />
+                              <div className="button-box">
+                                <div className="login-toggle-btn">
+                                  <input type="checkbox" />
+                                  <label className="ml-10">Remember me</label>
+                                  <Link to={process.env.PUBLIC_URL + "/"}>
+                                    Forgot Password?
+                                  </Link>
+                                </div>
+                                <button type="submit">
+                                  <span>Login</span>
+                                </button>
+                              </div>
+                            </form>
+                          </div>
+                        </div>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="register">
+                        <div className="login-form-container">
+                          <br />
+                          <div className="login-register-form">
+                            <form>
+                              <input
+                                type="text"
+                                name="user-name"
+                                placeholder="Username"
+                              />
+                              <input
+                                type="password"
+                                name="user-password"
+                                placeholder="Password"
+                              />
+                              <input
+                                name="user-email"
+                                placeholder="Email"
+                                type="email"
+                              />
+                              <div className="button-box">
+                                <button type="submit">
+                                  <span>Register</span>
+                                </button>
+                              </div>
+                            </form>
+                          </div>
+                        </div>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
                 </div>
               </div>
             </div>
           </div>
-        </LayoutOne>
-      </Fragment>
+        </div>
+      </LayoutOne>
+    </Fragment>
   );
 };
 
-LoginRegister.propTypes = {
-  location: PropTypes.object,
-  isLoggingIn: PropTypes.bool,
-  loginError: PropTypes.object,
-  errorMessage: PropTypes.object,
-  isAuthenticated: PropTypes.bool
-};
+ LoginRegister.propTypes = {
+   location: PropTypes.object,
+   isLoggingIn: PropTypes.bool,
+   loginError: PropTypes.object,
+   errorMessage: PropTypes.object,
+   isAuthenticated: PropTypes.bool
+ };
 
 function mapStateToProps(state) {
   return {
