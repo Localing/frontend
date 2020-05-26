faefe87dd4da4927b3efd9146379be0bb852dc41
Vishnu Ravi <vishnu.ravi@gmail.com>
2020-05-26 17:01:14 -0400

    Revert to prior login form, change auth domain

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -8,7 +8,7 @@ const awsmobile = {
     "aws_user_pools_id": "eu-west-2_8GjbfJ9Ct",
     "aws_user_pools_web_client_id": "u53qbta3dfjv74kfj9oeakujg",
     "oauth": {
-        "domain": "localing-prod.auth.eu-west-2.amazoncognito.com",
+        "domain": "auth.localing.co.uk",
         "scope": [
             "phone",
             "email",
