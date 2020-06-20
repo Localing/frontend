e9f7f26f97f774fe90e784dbe05f2e4ecfb2b7ee
Vishnu Ravi <vishnu.ravi@gmail.com>
2020-06-20 09:24:36 -0400

    Simplify grid code

--- src/components/business/BusinessGrid.js
+++ src/components/business/BusinessGrid.js
@@ -4,37 +4,27 @@ import { Link } from "react-router-dom";
 
 const BusinessGrid = ({ businesses }) => {
 
-  const renderBusinesses = () => {
-    const businessesToDisplay = [...businesses];
-    const numberPerRow = 2;
-    let rows = [];
-
-    while (businessesToDisplay.length) {
-      rows.push(businessesToDisplay.splice(0, numberPerRow));
-    }
-
-    return rows.map(row => (
-      <Row className="mt-4">{row.map(business =>
-        <Col md className="mt-4">
-          <Link to={`/business/${business.id}`} className="button-text w-inline-block">
-            <div className="business-content-wrap" style={{ backgroundImage: `url(${business.imageURL})` }}>
-              <div className="business-content-card-wrap">
-                <div className="business-name-wrap"><p className="size3-link">{business.name}</p></div>
-                <div className="button-text w-inline-block">
-                  <div className="button-label">EXPLORE PRODUCTS<img src="/assets/img/Arrow%402x.svg" alt="" className="button-arrow" />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </Link>
-        </Col>
-      )}</Row>
-    ))
-  }
-
   return (
     <Container>
-      {renderBusinesses()}
+      <Row className="mt-4">
+        {businesses.map(business => {
+          return (
+            <Col md={6} className="mt-4">
+              <Link to={`/business/${business.id}`} className="button-text w-inline-block">
+                <div className="business-content-wrap" style={{ backgroundImage: `url(${business.imageURL})` }}>
+                  <div className="business-content-card-wrap">
+                    <div className="business-name-wrap"><p className="size3-link">{business.name}</p></div>
+                    <div className="button-text w-inline-block">
+                      <div className="button-label">EXPLORE PRODUCTS<img src="/assets/img/Arrow%402x.svg" alt="" className="button-arrow" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            </Col>
+          )
+        })}
+      </Row>
     </Container>
   )
 }
