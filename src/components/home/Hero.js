cafc70d26157a187a0f3bb401593cd774a1f183a
Vishnu Ravi <vishnu.ravi@gmail.com>
2020-06-24 15:43:18 -0400

    New design/workflow for postcode entry

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -10,8 +10,6 @@ const Hero = ({ locationData, setLocation, clearLocationError, businesses }) => 
 
     const [business, setBusiness] = useState(randomBusiness());
 
-    const [showPostcodeForm, setShowPostcodeForm] = useState(true);
-
     // //Rotate businesses in hero
 
     // useEffect(() => {
@@ -24,17 +22,25 @@ const Hero = ({ locationData, setLocation, clearLocationError, businesses }) => 
     // handling changes to postcodes
     const [postcode, setPostcode] = useState("");
 
+    const [showPostcodeForm, setShowPostcodeForm] = useState(true);
+
     useEffect(() => {
+        // show the postcode form if there's no location set
         setShowPostcodeForm(!locationData.location)
     }, [locationData.location])
 
     const handlePostcodeSubmit = (event) => {
         event.preventDefault();
 
-        if(postcode === locationData.postcode){
+        // strip any whitespace out of postcode
+        const newPostcode = postcode.replace(/\s+/g, '');
+
+        if(newPostcode === locationData.postcode){
+            // if user enters the same postcode that's already set, clear any errors and hide the form
+            clearLocationError();
             setShowPostcodeForm(false);
         }else{
-            setLocation(postcode);
+            setLocation(newPostcode);
         }
     }
 
@@ -43,7 +49,7 @@ const Hero = ({ locationData, setLocation, clearLocationError, businesses }) => 
             <div className="w-layout-grid hero2-grid">
                 <div id="w-node-6d7d04cfb30d-5fcf7beb" className="hero2-content">
                     <div className="hero2-title">
-                        <div className="size1-text">Keep the heart of your community beating</div>
+                        <div className="size1-text">Keep the heart of {locationData.location || "your community"} beating</div>
                         <p class="paragraph-70">Pre-order from local businesses, and <strong>unlock exclusive promotions, discounts and rewards</strong> for being loyal to your community!</p>
                         {showPostcodeForm ?
                             <form onSubmit={handlePostcodeSubmit} className="mt-4">
@@ -53,7 +59,7 @@ const Hero = ({ locationData, setLocation, clearLocationError, businesses }) => 
                                     </Alert>
                                 }
                                 <div className="postcode-form">
-                                    <input type="text" name="postcode" placeholder="Enter your postcode" value={postcode} onChange={e => setPostcode(e.target.value)} required />
+                                    <input type="text" className="postcode-input mr-1" name="postcode" placeholder="Enter your postcode" value={postcode} onChange={e => setPostcode(e.target.value)} required />
                                     {locationData.loading ?
                                         <Spinner animation="border" role="status" size="lg">
                                         </Spinner>
@@ -61,8 +67,16 @@ const Hero = ({ locationData, setLocation, clearLocationError, businesses }) => 
                                 </div>
                             </form>
                             :
-                            <div>
-                                <h3 className="mt-2"><i className="fa fa-map-marker mr-1" />{locationData.location}<button className="button-small" onClick={() => setShowPostcodeForm(true)}>change</button></h3>
+                            <div className="location-display mt-4">
+                                <div className="location-name mr-1">
+                                    <i className="fa fa-map-marker mr-1 ml-1" />{locationData.location}
+                                </div>
+                                <div className="postcode-form mr-1">
+                                    <button className="button-small" onClick={() => setShowPostcodeForm(true)}>Change</button>
+                                </div>
+                                <div>
+                                    <button className="button-small" onClick={() => setShowPostcodeForm(true)}>Shop Now</button>
+                                </div>
                             </div>
                         }
                     </div>
