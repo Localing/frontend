96f236c312e0d143f353d608825310178277e24e
Vishnu Ravi <vishnu.ravi@gmail.com>
2020-06-26 07:42:56 -0400

    Add error handling to business actions/reducersq

--- src/redux/actions/businessActions.js
+++ src/redux/actions/businessActions.js
@@ -1,22 +1,34 @@
 import axios from "axios";
 
+export const FETCH_BUSINESSES_BEGIN ="FETCH_BUSINESSES_BEGIN";
 export const FETCH_BUSINESSES_SUCCESS = "FETCH_BUSINESSES_SUCCESS";
+export const FETCH_BUSINESSES_ERROR = "FETCH_BUSINESSES_ERROR";
 
 const fetchBusinessesSuccess = businesses => ({
   type: FETCH_BUSINESSES_SUCCESS,
   payload: businesses
 });
 
+const fetchBusinessesError = error => ({
+  type: FETCH_BUSINESSES_ERROR,
+  payload: error
+});
+
+const fetchBusinessesBegin = () => ({
+  type: FETCH_BUSINESSES_BEGIN
+});
+
 // fetch businesses
 export const fetchBusinesses = () => {
   return dispatch => {
+    dispatch(fetchBusinessesBegin());
     axios
       .get(`https://consumerapi.dev.localing.co.uk/business/`)
       .then(response => {
         dispatch(fetchBusinessesSuccess(response.data));
       })
-      .catch(err => {
-        console.log(err);
+      .catch(error => {
+        dispatch(fetchBusinessesError(error));
       });  
   };
 };
