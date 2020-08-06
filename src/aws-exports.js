25d7cfb49ce8f7c476cb9d6326e97ee8fd348df4
Vishnu Ravi <vishnu.ravi@gmail.com>
2020-08-06 12:54:58 -0400

    Configure auth for multiple environments

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,6 +1,9 @@
 /* eslint-disable */
 
-const awsmobile = {
+/* AWS Amplify Configuration */
+
+/* Development */
+const dev_config = {
     "aws_project_region": "eu-west-2",
     "aws_cognito_region": "eu-west-2",
     "aws_user_pools_id": "eu-west-2_k7xQLLSPp",
@@ -14,11 +17,57 @@ const awsmobile = {
             "profile",
             "aws.cognito.signin.user.admin"
         ],
-        "redirectSignIn": (process.env.REACT_APP_BuildENV) ? ((process.env.REACT_APP_BuildENV === "prod") ? "https://prod.localing.co.uk" : "https://stage.localing.co.uk/") : "http://localhost:3000/",
-        "redirectSignOut": (process.env.REACT_APP_BuildENV) ? ((process.env.REACT_APP_BuildENV === "prod") ? "https://prod.localing.co.uk" : "https://stage.localing.co.uk/") : "http://localhost:3000/",
+        "redirectSignIn": "http://localhost:3000/",
+        "redirectSignOut": "http://localhost:3000/",
         "responseType": "code"
     },
     "federationTarget": "COGNITO_USER_POOLS"
 };
 
-export default awsmobile;
+/* Staging */
+const stage_config = {
+    "aws_project_region": "eu-west-2",
+    "aws_cognito_region": "eu-west-2",
+    "aws_user_pools_id": "eu-west-2_ldxMX1FqA",
+    "aws_user_pools_web_client_id": "7c6cmggqbftl4874u5dcvka2rq",
+    "oauth": {
+        "domain": "auth.localing.co.uk",
+        "scope": [
+            "phone",
+            "email",
+            "openid",
+            "profile",
+            "aws.cognito.signin.user.admin"
+        ],
+        "redirectSignIn": "https://stage.localing.co.uk/",
+        "redirectSignOut": "https://stage.localing.co.uk/",
+        "responseType": "code"
+    },
+    "federationTarget": "COGNITO_USER_POOLS"
+};
+
+/* Production */
+const prod_config = {
+    "aws_project_region": "eu-west-2",
+    "aws_cognito_region": "eu-west-2",
+    "aws_user_pools_id": "eu-west-2_OB4xNIeGr",
+    "aws_user_pools_web_client_id": "2qkm5814fdn4pvoikioan1nvub",
+    "oauth": {
+        "domain": "auth.localing.co.uk",
+        "scope": [
+            "phone",
+            "email",
+            "openid",
+            "profile",
+            "aws.cognito.signin.user.admin"
+        ],
+        "redirectSignIn":  "https://prod.localing.co.uk",
+        "redirectSignOut": "https://prod.localing.co.uk",
+        "responseType": "code"
+    },
+    "federationTarget": "COGNITO_USER_POOLS"
+};
+
+const awsconfig = (process.env.REACT_APP_BuildENV) ? ((process.env.REACT_APP_BuildENV === "prod") ? prod_config : stage_config) : dev_config;
+
+export default awsconfig;
