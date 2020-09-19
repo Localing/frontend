17c70c46382977bbd23ac749d79259219f533eef
Vishnu Ravi <vishnu.ravi@gmail.com>
2020-09-18 22:36:54 -0400

    Bugfixes (#139)
    * Update API URIs
    
    * Fixes issue #127
    
    * Adds placeholders for missing product images

--- src/helpers/product.js
+++ src/helpers/product.js
@@ -28,7 +28,7 @@ export const getProducts = (products, category, type, limit) => {
 
 // get product discount price
 export const getDiscountPrice = (price, discount) => {
-  return discount && discount > 0 ? price - price * (discount / 100) : null;
+  return discount && discount > 0 ? Math.round(price - price * (discount / 100)) : null;
 };
 
 // get product cart quantity
